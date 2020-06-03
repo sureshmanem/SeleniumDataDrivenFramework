aadbd0e9039b9a76cbfc0a60abb87a9183128d2f
Zurezh <sr.manem@gmail.com>
2020-06-03 18:46:48 +0530

    Before First Execution

---
+++ DataDrivenFramework/src/test/java/com/suresh/testcases/OpenAccountTest.java
@@ -0,0 +1,37 @@
+package com.suresh.testcases;
+
+import org.testng.annotations.Test;
+import java.util.Hashtable;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+import com.suresh.base.TestBase;
+import com.suresh.utilities.TestUtil;
+
+public class OpenAccountTest extends TestBase {
+
+	@Test(dataProviderClass = TestUtil.class, dataProvider = "dp")
+	public void openAccountTest(Hashtable<String,String> data) throws InterruptedException {
+
+		
+		if(!(TestUtil.isTestRunnable("openAccountTest", excel))){
+			
+			throw new SkipException("Skipping the test "+"openAccountTest".toUpperCase()+ "as the Run mode is NO");
+		}
+		
+		
+		click("openaccount_CSS");
+		select("customer_CSS", data.get("customer"));
+		select("currency_CSS", data.get("currency"));
+		click("process_CSS");
+		Thread.sleep(2000);
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		alert.accept();
+
+	}
+
+}
